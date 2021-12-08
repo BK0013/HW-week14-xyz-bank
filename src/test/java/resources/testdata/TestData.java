3e306058780398807cc847f2417f7318f1ff671a
Bagajera13 <bharatgajera.8@gmail.com>
2021-12-08 16:08:05 +0000

    week14 xyz bank

--- src/test/java/resources/testdata/TestData.java
+++ src/test/java/resources/testdata/TestData.java
@@ -1,4 +1,17 @@
 package resources.testdata;
 
+
+import org.testng.annotations.DataProvider;
+
 public class TestData {
+    @DataProvider(name = "credentials")
+    public Object[][] getData(){
+        return new Object[][]{
+                {"BK","Patel","London"},
+                {"Rudra","Patel","Wembley"},
+                {"Jay","Patel","London"},
+                {"Harsh","Patel","Wembley"}
+
+        };
+    }
 }
